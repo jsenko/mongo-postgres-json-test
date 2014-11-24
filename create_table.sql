ea745be32ee79807a2fbac23dad4a72207caeaae
tomasskopal <tomasskopal01@gmail.com>
2014-11-24 22:11:48 +0100

    add inserts into jsonb column

--- create_table.sql
+++ create_table.sql
@@ -2,3 +2,8 @@ CREATE TABLE json_data (
     id   SERIAL PRIMARY KEY,
     data JSON
 );
+
+CREATE TABLE jsonb_data (
+    id   SERIAL PRIMARY KEY,
+    data JSONB
+);
--- postgres_insert.py
+++ postgres_insert.py
@@ -4,14 +4,15 @@ import psycopg2
 import time
 from pymongo import *
 
+print
+
 try:
     conn = psycopg2.connect("dbname='no_sql' user='postgres' host='localhost' password='postgres'")
-    print "coccesfully connect"
 except:
     print "I am unable to connect to the database"
 
+#json
 cur = conn.cursor()
-
 cur.execute('TRUNCATE json_data CASCADE')
 query =  "INSERT INTO json_data (data) values ({});"
 n = 1000
@@ -22,8 +23,23 @@ for i in range(n):
 start = time.time()
 conn.commit()
 end = time.time()
-print 'Commit %d inserts into postgres took %0.3f ms' % (n, (end-start)*1000.0)
+print 'Commit %d inserts into postgres with json took %0.3f ms' % (n, (end-start)*1000.0)
 
+#json_b
+cur = conn.cursor()
+cur.execute('TRUNCATE jsonb_data CASCADE')
+query =  "INSERT INTO jsonb_data (data) values ({});"
+n = 1000
+for i in range(n):
+	data = '\'{"name": "Apple Phone", "type": "phone"}\''
+	cur.execute(query.format(data))
+
+start = time.time()
+conn.commit()
+end = time.time()
+print 'Commit %d inserts into postgres with jsonb took %0.3f ms' % (n, (end-start)*1000.0)
+
+#mongo
 
 client = MongoClient()
 collection = client.test.test
@@ -34,11 +50,12 @@ start = time.time()
 
 data = ""
 for i in range(n):
-	collection.insert({"name": "James", "surname": "Bond"})
+	collection.insert({"name": "Apple Phone", "type": "phone"})
 
 end = time.time()
 print '%d inserts in Mongo took %0.3f ms' % (n, (end-start)*1000.0)
 
+print
 print "End of the script"
 
 
