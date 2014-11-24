0ec0ecfedc5c468852ecd72b9e5f596fa8ccc84d
tomasskopal <tomasskopal01@gmail.com>
2014-11-24 15:47:40 +0100

    add mongos part to the script, but functionality is not complete yet.

--- postgres_insert.py
+++ postgres_insert.py
@@ -1,6 +1,8 @@
 __author__ = 'Tomas Skopal'
 
 import psycopg2
+import time
+from pymongo import *
 
 try:
     conn = psycopg2.connect("dbname='no_sql' user='postgres' host='localhost' password='postgres'")
@@ -12,10 +14,29 @@ cur = conn.cursor()
 
 cur.execute('TRUNCATE json_data CASCADE')
 query =  "INSERT INTO json_data (id, data) values (%s, %s);"
-for i in range(100):
+n = 1000
+for i in range(n):
 	data = (str(i), '{"name": "Apple Phone", "type": "phone"}')
 	cur.execute(query, data)
+
+start = time.time()
 conn.commit()
+end = time.time()
+print 'Commit %d inserts into postgres took %0.3f ms' % (n, (end-start)*1000.0)
+
+
+client = MongoClient()
+collection = client.test.test
+
+collection.remove()
+
+start = time.time()
+
+for i in range(n):
+	collection.insert({"name": "James", "surname": "Bond"})
+
+end = time.time()
+print '%d inserts in Mongo took %0.3f ms' % (n, (end-start)*1000.0)
 
 print "End of the script"
 
